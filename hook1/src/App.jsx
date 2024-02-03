1b1cdd6307625c2dca371ac5c71803aec037ab2d
Nayan Kumar <naayaankumar@gmail.com>
2024-02-03 18:02:00 +0530

    updated the file

--- hook1/src/App.jsx
+++ hook1/src/App.jsx
@@ -4,6 +4,7 @@ import State4 from './Component/Progression4';
 import State3 from './Component/Progression4';
 import Hookeffect1 from './Component/Progression5';
 import Toggle from './Component/Progression6';
+import Task from './Component/task';
 
 function App() {
   //useState
@@ -18,6 +19,7 @@ function App() {
       <State4 />
       <Hookeffect1 /> */}
       <Toggle />
+      <Task />
     </div>
   );
 }
